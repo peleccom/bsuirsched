62234da0f3914dda54cbcc61c3c7e7a8d33ca942
Alexander Pitkin <peleccom@gmail.com>
2013-09-18 12:40:19 +0300

    Added get_groups_list function

--- bsuirschedule/bsuirparser.py
+++ bsuirschedule/bsuirparser.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-
+import cookielib
 
 from lxml import etree
 import urllib
+import urllib2
 import logging
 import datetime
 import models
+import json
 
 
 def fetch(group):
@@ -23,7 +25,7 @@ def fetch(group):
         else:
             return None
     except Exception, e:
-        logging.error("Fetching error %s" %(str(e)))
+        logging.error("Fetching error %s" % (str(e)))
         return None
 
 
@@ -39,26 +41,25 @@ def parse(tablestring):
     st_days = []
     for day in days:
         name = day.xpath("td[1]")[0]
-##        print name.text
+        ##        print name.text
         weeks = [week.text for week in day.xpath("td[2]")[0]]
-##        print weeks
+        ##        print weeks
         times = [time.text for time in day.xpath("td[3]")[0]]
-##        print times
+        ##        print times
         subgroups = [subgroup.text for subgroup in day.xpath("td[4]")[0]]
-##        print subgroups
+        ##        print subgroups
         subjects = [subject.text for subject in day.xpath("td[5]")[0]]
-##        print subjects
+        ##        print subjects
         lesson_types = [lessontype.text for lessontype in day.xpath("td[6]")[0]]
-##        print lessontypes
+        ##        print lessontypes
         places = [place.text for place in day.xpath("td[7]")[0]]
-##        print places
+        ##        print places
         lecturers = [lecturer.text for lecturer in day.xpath("td[8]")[0]]
-##        print lecturers
+        ##        print lecturers
         lessons = []
         for i in range(len(subjects)):
-
             les = models.Lesson(weeks[i], times[i], subgroups[i], subjects[i],
-                                lesson_types[i], places[i], lecturers[i])
+                lesson_types[i], places[i], lecturers[i])
             lessons.append(les)
         st_day = models.StudyDay(lessons, name.text)
         st_days.append(st_day)
@@ -72,10 +73,10 @@ def get_week_num(year, month, day):
         if month >= 9:
             startday = datetime.date(year, 9, 1)
         else:
-            startday = datetime.date(year-1, 9, 1)
+            startday = datetime.date(year - 1, 9, 1)
         newweek = datetime.date(year, month, day).isocalendar()[1]
         startweek = startday.isocalendar()[1]
-        return ((newweek - startweek) % 4) +1
+        return ((newweek - startweek) % 4) + 1
     except Exception:
         return None
 
@@ -84,7 +85,7 @@ def subgroup2int(subgroup_str):
     """Convert subgroup string to int with Value Error handling"""
     try:
         subgroup = int(subgroup_str)
-        if not 1<= subgroup <= 2:
+        if not 1 <= subgroup <= 2:
             raise ValueError("Subgroup must be 1-2")
     except Exception, e:
         subgroup = None
@@ -100,3 +101,76 @@ def week2int(week_str):
     except Exception, e:
         week = None
     return week
+
+
+def get_groups_list():
+    """Return list of groups from bsuir schedule page"""
+    try:
+        result_groups = []
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
+        url = "http://www.bsuir.by/psched/schedulegroup.tab2_link"
+        select_faculty_url = "http://www.bsuir.by/psched/schedulegroup.faculty:change"
+        select_course_url = "http://www.bsuir.by/psched/schedulegroup.course:change"
+        select_flow_url = "http://www.bsuir.by/psched/schedulegroup.flow:change"
+        post_params = {
+            "t:zoneid":"zoneFlow",
+            "t:formid":"filterForm",
+            "t:formcomponentid":"ScheduleGroup:filterform",
+            "t:selectvalue":"34330000000000000000000000"
+                    }
+        headers = {'X-Prototype-Version':'1.7',
+                   'X-Requested-With':'XMLHttpRequest'}
+        faculties_list_request = urllib2.Request(url)
+        raw_html = opener.open(faculties_list_request).read()
+        parser = etree.HTMLParser(encoding="utf8")
+        root = etree.HTML(raw_html, parser=parser)
+        faculties = root.xpath(".//select[@id='faculty']/option")
+        for faculty in faculties:
+            ## перебираем факультеты
+            #print faculty.text
+            #установка параметра факультет
+            post_params['t:selectvalue'] = faculty.attrib.get("value")
+            #запрос факультет
+            groups_list_request = urllib2.Request(select_faculty_url, urllib.urlencode(post_params), headers)
+#            print post_params
+            s_json_value = opener.open(groups_list_request).read()
+            #получаем ответ в json
+            try:
+                json_value = json.loads(s_json_value)
+                html = json_value['zones']['zoneCourse']
+                courses_root = etree.HTML(html, parser=parser)
+                options = courses_root.xpath(".//option")
+                courses = [option.attrib.get("value") for option in options]
+                for course in courses:
+                    #print "\tcourse=", course
+                    post_params["t:selectvalue"] = course
+                    #запрос курса
+                    groups_list_request = urllib2.Request(select_course_url, urllib.urlencode(post_params), headers)
+#                    print post_params
+                    s_json_value = opener.open(groups_list_request).read()
+                    json_value_flow =  json.loads(s_json_value)
+                    html = json_value_flow['zones']['zoneFlow']
+                    courses_root = etree.HTML(html, parser=parser)
+                    options = courses_root.xpath(".//option")
+                    flows = [option.attrib.get("value") for option in options]
+                    for flow in flows:
+                        #print "\t\tflow=",flow
+                        post_params["t:selectvalue"] = flow
+                        #запрос курса
+                        groups_list_request = urllib2.Request(select_flow_url, urllib.urlencode(post_params), headers)
+                        #                    print post_params
+                        s_json_value = opener.open(groups_list_request).read()
+                        json_value_flow =  json.loads(s_json_value)
+                        html = json_value_flow['zones']['zoneGroup']
+                        groups_root = etree.HTML(html, parser=parser)
+                        group_options = groups_root.xpath(".//option")
+                        groups  = [group_option.text for group_option in  group_options]
+                        for group in groups:
+                            #print "\t\t\tgroup=", group
+                            result_groups.append(group)
+            except ValueError, e:
+                logging.error(e)
+        return result_groups
+    except Exception, e:
+        logging.error("Fetching groups error %s" % (str(e)))
+        return None
