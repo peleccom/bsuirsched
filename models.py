9703f9581e8c5d10925630901ec19e348f330cbf
Alexander Pitkin <peleccom@gmail.com>
2013-09-19 23:43:41 +0300

    Added GroupList db kind

--- models.py
+++ models.py
@@ -6,3 +6,6 @@ class GroupSchedule(ndb.Model):
     group = ndb.StringProperty(required=True)
     schedule = ndb.PickleProperty()
     date = ndb.DateTimeProperty(auto_now=True)
+
+class GroupsList(ndb.Model):
+    groups_list = ndb.PickleProperty(required=True)
